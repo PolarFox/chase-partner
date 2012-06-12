c340ef646c8fc28340216e155d80c70a77720a8b
Mikko Ruohola <mikko@ruohola.org>
2012-06-12 15:07:39 +0300

    License info

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ chase-partner
 =============
 
 Storm chaser location service to keep teams aware of each others location - privately.
+
+This software is licensed under GPL V3 http://www.opensource.org/licenses/GPL-3.0
+And built on top of Ramaze (http://ramaze.net/)
+
--- controller/main.rb
+++ controller/main.rb
@@ -1,22 +1,26 @@
-# Default url mappings are:
-# 
-# * a controller called Main is mapped on the root of the site: /
-# * a controller called Something is mapped on: /something
-# 
-# If you want to override this, add a line like this inside the class:
-#
-#  map '/otherurl'
-#
-# this will force the controller to be mounted on: /otherurl.
+=begin
+    ChasePartner gives teams of people tools to keep track of each others locations, securely as possible.
+
+    Copyright (C) 2012 Mikko Ruohola <mikko@ruohola.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
 class MainController < Controller
-  # the index action is called automatically when no other action is specified
   def index
     @title = 'Welcome to ChasePartner!'
   end
 
-  # the string returned at the end of the function is used as the html body
-  # if there is no template for the action. if there is a template, the string
-  # is silently ignored
   def notemplate
     @title = 'Welcome to ChasePartner!'
     
